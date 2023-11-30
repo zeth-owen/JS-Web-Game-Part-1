57cf4cd0289f7a18e2329aac89a50d3afe0ad550
Zeth Thomas <zeththomas89@gmail.com>
2023-11-30 13:01:12 -0500

    newImages & newItems added

--- index.js
+++ index.js
@@ -1,6 +1,37 @@
-let greenCharacter = document.createElement('img')
-greenCharacter.src = 'assets/green-character.gif'
-greenCharacter.style.position = 'fixed'
-greenCharacter.style.left = '100px'
-greenCharacter.style.bottom = '100px'
-document.body.append(greenCharacter)
+
+//Function for Character & Static Imagery
+function newImage(src, left, bottom) {
+    let character = document.createElement('img');
+    character.src = src;
+    character.style.position = 'fixed';
+    character.style.left = left;
+    character.style.bottom = bottom;
+    document.body.append(character);
+return character;
+}
+
+//Function for User Items
+function newItem(src, left, bottom) {
+    let item = newImage(src, left, bottom);
+     
+    item.addEventListener('dblclick', () => {
+        item.remove();
+    })
+}
+
+
+//Evoking Functions w/ arguments
+newImage('assets/green-character.gif', '100px', '100px');
+newImage('assets/pine-tree.png','450px','200px');
+newImage('assets/tree.png', '200px', '300px');
+newImage('assets/pillar.png','350px','100px');
+newImage('assets/crate.png','150px','200px');
+newImage('assets/well.png','500px','425px')
+
+newItem('assets/sword.png','500px','405px');
+newItem('assets/shield.png','165px','185px');
+newItem('assets/staff.png','600px','100px');
+
+
+
+
