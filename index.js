01de3f049d931aa7c8797c48636433646c41b52c
Zeth Thomas <zeththomas89@gmail.com>
2023-11-30 20:46:08 -0500

    Sky/grass attempt

--- index.js
+++ index.js
@@ -1,11 +1,19 @@
+//Function for Background
+function bgImage(url, left, bottom, width, height) {
+    for(let h = 0; h < height; h++) {
+        for(let w = 0; w < width; w++) {
+            newImage(url, left + w* 100, bottom + h * 100);
+        }
+    }
+}
 
 //Function for Character & Static Imagery
 function newImage(src, left, bottom) {
     let character = document.createElement('img');
     character.src = src;
     character.style.position = 'fixed';
-    character.style.left = left;
-    character.style.bottom = bottom;
+    character.style.left = left + 'px';
+    character.style.bottom = bottom + 'px';
     document.body.append(character);
 return character;
 }
@@ -19,6 +27,10 @@ function newItem(src, left, bottom) {
     })
 }
 
+//Assigning value to variables in Background Arguments
+let horizon = window.innerHeight / 1.75
+let heightOfSky = window.innerHeight-horizon
+let heightOfGrass = horizon
 
 //Evoking Functions w/ arguments
 newImage('assets/green-character.gif', '100px', '100px');
@@ -32,6 +44,5 @@ newItem('assets/sword.png','500px','405px');
 newItem('assets/shield.png','165px','185px');
 newItem('assets/staff.png','600px','100px');
 
-
-
-
+bgImage('assets/grass.png',0,0,window.innerWidth/100, heightOfGrass/100);
+bgImage('assets/sky.png', 0, horizon, window.innerWidth/100, heightOfSky/100);clear
